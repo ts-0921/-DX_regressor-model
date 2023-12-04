3b66473c9ff8c5bf10468090259c6937a845cf5c
ts-0921 <152828124+ts-0921@users.noreply.github.com>
2023-12-04 14:14:33 +0900

    Add files via upload

--- 重回帰アプリ.py
+++ 重回帰アプリ.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np
 import streamlit as st
-import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
